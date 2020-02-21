e8b218513f1657967eb9c7d2cd557676aa9b2520
Bartek Wiekiera <bartoleospl@gmail.com>
2020-02-21 17:47:57 +0100

    dodanie projektu operacje logiczne

--- Solucja/4. operacje logiczne/Program.cs
+++ Solucja/4. operacje logiczne/Program.cs
@@ -1,12 +1,15 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace _4._operacje_logiczne
+namespace _4.Operacje_logiczne
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
         }
     }
 }
