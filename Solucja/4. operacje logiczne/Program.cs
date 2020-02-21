9b36d7a346d4c1a3f2acb49252f0f3f03e673e17
Bartek Wiekiera <bartoleospl@gmail.com>
2020-02-21 18:29:45 +0100

    Program losujący liczbę od 1 do 100 i podpowiada czy wprowadzona iczba jest wieksza czy mniejsza aż do odgadnięcia

--- Solucja/4. operacje logiczne/Program.cs
+++ Solucja/4. operacje logiczne/Program.cs
@@ -10,6 +10,55 @@ namespace _4.Operacje_logiczne
     {
         static void Main(string[] args)
         {
+
+            int randomNumber = 0;
+            Random random = new Random();
+
+            randomNumber = random.Next(0, 100);
+
+            var exit = true;
+            
+            while(exit)
+            {
+                Console.WriteLine("Podaj liczbę od 0 do 100");
+                var input = Console.ReadLine();
+
+                var number = 0;
+                var result = int.TryParse(input, out number);
+
+                if (result)
+                {
+                    if (number >= 0 && number <= 100)
+                    {
+                        if (number < randomNumber)
+                        {
+                            Console.WriteLine("Twoja liczba jest mniejsza od wylosowanej");
+                        }
+
+                        if (number > randomNumber)
+                        {
+                            Console.WriteLine("Twoja liczba jest większa od wylosowanej");
+                        }
+
+                        if (number == randomNumber)
+                        {
+                            Console.WriteLine("Twoja liczba równa co wyloswana");
+                            exit = false;
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Wpisałeś liczbę z poza zakresu od 0 do 100");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Nie wpisaleś liczby");
+                }
+
+            }
+
+            Console.ReadLine();
         }
     }
 }
